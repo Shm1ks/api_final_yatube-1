2d2fe3a503204c3426bd34535526fc04bfea376d
Евгений Волочек <105015075+EvgVol@users.noreply.github.com>
2022-10-13 16:03:07 +0300

    v.1

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,16 +1,57 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from django.core.files.base import ContentFile
+
+from posts.models import Comment, Post, Follow, Group
+
+import base64
 
 
-from posts.models import Comment, Post
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+
+        return super().to_internal_value(data)
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
+    image = Base64ImageField(required=False, allow_null=True)
 
     class Meta:
         fields = '__all__'
         model = Post
+    
+    def update(self, instance, validated_data):
+        instance.text = validated_data.get('text', instance.text)
+        instance.author = validated_data.get('author', instance.color)
+        instance.birth_year = validated_data.get(
+            'birth_year', instance.birth_year
+            )
+        instance.image = validated_data.get('image', instance.image)
+        if 'achievements' in validated_data:
+            achievements_data = validated_data.pop('achievements')
+            lst = []
+            for achievement in achievements_data:
+                current_achievement, status = Achievement.objects.get_or_create(
+                    **achievement
+                    )
+                lst.append(current_achievement)
+            instance.achievements.set(lst)
+
+        instance.save()
+        return instance
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -21,3 +62,13 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,48 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import (ReadOnlyModelViewSet,
+                                     ModelViewSet)
+
+from .serializers import (FollowSerializer,
+                          PostSerializer,
+                          GroupSerializer,
+                          CommentSerializer)
+from .permissions import IsAuthor
+from posts.models import Post, Group
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthor]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsAuthor]
+
+    def get_post(self):
+        return get_object_or_404(
+            Post, id=self.kwargs.get('post_id')
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user, post=self.get_post()
+        )
+
+    def get_queryset(self):
+        return self.get_post().comments.all()
+
+
+class FollowViewSet(ModelViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated]
